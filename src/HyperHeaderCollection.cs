f7e5137b535258dbef52e937cf7a4d6990976e57
Lunar Starstrum <lunar@forsaken-borders.net>
2023-07-12 09:18:35 -0500

    Header name and value validation

--- src/HyperHeaderCollection.cs
+++ src/HyperHeaderCollection.cs
@@ -2,16 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 
 namespace OoLunar.HyperSharp
 {
     public sealed class HyperHeaderCollection : IReadOnlyDictionary<string, IReadOnlyList<string>>
     {
-        private readonly Dictionary<string, List<string>> Headers;
+        // https://developers.cloudflare.com/rules/transform/request-header-modification/reference/header-format
+        private static readonly char[] ValidHeaderNameCharacters = new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '-', '_'
+        };
+
+        private static readonly char[] ValidHeaderValueCharacters = new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            '-', '_', ' ', ':', ';', '.', ',', '\\', '/', '"', '\'', '!', '?', '(', ')', '{', '}', '[', ']', '@', '<', '>', '=', '+', '*', '#', '$', '&', '`', '|', '~', '^', '%'
+        };
+
         public IEnumerable<string> Keys => Headers.Keys;
         public IEnumerable<IReadOnlyList<string>> Values => Headers.Values;
         public int Count => Headers.Count;
+
+        private readonly Dictionary<string, List<string>> Headers;
 
         public HyperHeaderCollection() => Headers = new();
         public HyperHeaderCollection(IDictionary<string, List<string>> dictionary, IEqualityComparer<string>? comparer = null) => Headers = new Dictionary<string, List<string>>(dictionary, comparer);
@@ -34,15 +51,25 @@ namespace OoLunar.HyperSharp
 
         public IEnumerator<KeyValuePair<string, IReadOnlyList<string>>> GetEnumerator()
         {
-            IEnumerator<KeyValuePair<string, List<string>>> enumerator = Headers.GetEnumerator();
-            return Unsafe.As<IEnumerator<KeyValuePair<string, List<string>>>, IEnumerator<KeyValuePair<string, IReadOnlyList<string>>>>(ref enumerator);
+            foreach (KeyValuePair<string, List<string>> header in Headers)
+            {
+                yield return new KeyValuePair<string, IReadOnlyList<string>>(header.Key, header.Value.AsReadOnly());
+            }
         }
         IEnumerator<KeyValuePair<string, IReadOnlyList<string>>> IEnumerable<KeyValuePair<string, IReadOnlyList<string>>>.GetEnumerator() => GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public void AddHeaderValue(string name, string value)
         {
-            ArgumentException.ThrowIfNullOrEmpty(value, nameof(value));
+            if (!IsValidHeaderName(name))
+            {
+                throw new ArgumentException("Header name is invalid.", nameof(name));
+            }
+            else if (!IsValidHeaderValue(value))
+            {
+                throw new ArgumentException("Header value is invalid.", nameof(value));
+            }
+
             if (!Headers.TryGetValue(name, out List<string>? values))
             {
                 values = new();
@@ -54,10 +81,19 @@ namespace OoLunar.HyperSharp
 
         public void AddHeaderValues(string name, IEnumerable<string> values)
         {
+            if (!IsValidHeaderName(name))
+            {
+                throw new ArgumentException("Header name is invalid.", nameof(name));
+            }
+
             List<string> newValues = new();
             foreach (string value in values)
             {
-                ArgumentException.ThrowIfNullOrEmpty(value, nameof(values));
+                if (!IsValidHeaderValue(value))
+                {
+                    throw new ArgumentException("Header value is invalid.", nameof(values));
+                }
+
                 newValues.Add(value.Trim());
             }
 
@@ -72,5 +108,43 @@ namespace OoLunar.HyperSharp
 
         public void SetHeader(string name, IEnumerable<string> values) => Headers[name] = new List<string>(values);
         public void RemoveHeader(string name) => Headers.Remove(name);
+
+        public static bool IsValidHeaderName(string value)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+
+            Span<char> validNameSpan = ValidHeaderNameCharacters.AsSpan();
+            foreach (char c in value)
+            {
+                if (validNameSpan.IndexOf(c) == -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsValidHeaderValue(string value)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+
+            Span<char> validValueSpan = ValidHeaderValueCharacters.AsSpan();
+            foreach (char c in value)
+            {
+                if (validValueSpan.IndexOf(c) == -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
