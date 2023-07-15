b51bd94c36fbf47ecb1791b0f39b8f3fda764667
Lunar Starstrum <lunar@forsaken-borders.net>
2023-07-15 12:52:34 -0500

    Rename variables and override ToString

--- src/HyperContext.cs
+++ src/HyperContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Frozen;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Net;
 using System.Net.Http;
@@ -13,6 +14,7 @@ namespace OoLunar.HyperSharp
 {
     public sealed class HyperContext
     {
+        [DebuggerDisplay("{ToString(),nq}")]
         private static readonly FrozenDictionary<Version, byte[]> HttpVersions = new Dictionary<Version, byte[]>()
         {
             [HttpVersion.Version10] = "HTTP/1.0 "u8.ToArray(),
@@ -23,63 +25,65 @@ namespace OoLunar.HyperSharp
         public Uri Route { get; init; }
         public Version Version { get; init; }
         public HyperHeaderCollection Headers { get; init; }
-        public PipeReader Body { get; init; }
+        public PipeReader BodyReader { get; init; }
 
         public Dictionary<string, string> Metadata { get; init; } = new();
         public bool HasResponded { get; private set; }
 
-        private PipeWriter Responder { get; init; }
+        private PipeWriter ResponseWriter { get; init; }
 
-        public HyperContext(HttpMethod method, Uri route, Version version, HyperHeaderCollection headers, PipeReader body, PipeWriter responder)
+        public HyperContext(HttpMethod method, Uri route, Version version, HyperHeaderCollection headers, PipeReader bodyReader, PipeWriter responseWriter)
         {
             Version = version ?? throw new ArgumentNullException(nameof(version));
             Method = method ?? throw new ArgumentNullException(nameof(method));
             Route = route ?? throw new ArgumentNullException(nameof(route));
             Headers = headers ?? throw new ArgumentNullException(nameof(headers));
-            Body = body ?? throw new ArgumentNullException(nameof(body));
-            Responder = responder ?? throw new ArgumentNullException(nameof(responder));
+            BodyReader = bodyReader ?? throw new ArgumentNullException(nameof(bodyReader));
+            ResponseWriter = responseWriter ?? throw new ArgumentNullException(nameof(responseWriter));
         }
 
         public async Task RespondAsync(HyperStatus status, JsonSerializerOptions? serializerOptions = null)
         {
             // Write request line
-            await Responder.WriteAsync(HttpVersions[Version]);
-            await Responder.WriteAsync(Encoding.ASCII.GetBytes($"{(int)status.Code} {status.Code}"));
-            await Responder.WriteAsync("\r\n"u8.ToArray());
+            await ResponseWriter.WriteAsync(HttpVersions[Version]);
+            await ResponseWriter.WriteAsync(Encoding.ASCII.GetBytes($"{(int)status.Code} {status.Code}"));
+            await ResponseWriter.WriteAsync("\r\n"u8.ToArray());
 
             // Write headers
             if (status.Headers is not null)
             {
                 foreach (KeyValuePair<string, IReadOnlyList<string>> header in status.Headers)
                 {
-                    await Responder.WriteAsync(Encoding.ASCII.GetBytes(header.Key));
-                    await Responder.WriteAsync(": "u8.ToArray());
+                    await ResponseWriter.WriteAsync(Encoding.ASCII.GetBytes(header.Key));
+                    await ResponseWriter.WriteAsync(": "u8.ToArray());
                     if (header.Value.Count == 1)
                     {
-                        await Responder.WriteAsync(Encoding.ASCII.GetBytes(header.Value[0]));
+                        await ResponseWriter.WriteAsync(Encoding.ASCII.GetBytes(header.Value[0]));
                     }
                     else
                     {
                         foreach (string value in header.Value)
                         {
-                            await Responder.WriteAsync(Encoding.ASCII.GetBytes(value));
-                            await Responder.WriteAsync(", "u8.ToArray());
+                            await ResponseWriter.WriteAsync(Encoding.ASCII.GetBytes(value));
+                            await ResponseWriter.WriteAsync(", "u8.ToArray());
                         }
                     }
 
-                    await Responder.WriteAsync("\r\n"u8.ToArray());
+                    await ResponseWriter.WriteAsync("\r\n"u8.ToArray());
                 }
             }
-            await Responder.WriteAsync("\r\n"u8.ToArray());
+            await ResponseWriter.WriteAsync("\r\n"u8.ToArray());
 
             // Write body
             if (status.Body is not null)
             {
-                await JsonSerializer.SerializeAsync(Responder.AsStream(), status.Body, serializerOptions ?? new JsonSerializerOptions(JsonSerializerDefaults.Web));
+                await JsonSerializer.SerializeAsync(ResponseWriter.AsStream(true), status.Body, serializerOptions ?? new JsonSerializerOptions(JsonSerializerDefaults.Web));
             }
 
-            await Responder.CompleteAsync();
+            await ResponseWriter.CompleteAsync();
             HasResponded = true;
         }
+
+        public override string ToString() => $"{Method} {Route} HTTP {Version}, {Headers.Count:N0} headers";
     }
 }
--- src/HyperStatus.cs
+++ src/HyperStatus.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Diagnostics;
 using System.Net;
 using OoLunar.HyperSharp.Parsing;
 
 namespace OoLunar.HyperSharp
 {
-    public readonly record struct HyperStatus(HttpStatusCode Code, HyperHeaderCollection? Headers = null, object? Body = null);
+    [DebuggerDisplay("{ToString(),nq}")]
+    public readonly record struct HyperStatus
+    {
+        public HttpStatusCode Code { get; init; }
+        public HyperHeaderCollection Headers { get; init; }
+        public object? Body { get; init; }
+
+        public HyperStatus(HttpStatusCode code) : this(code, new HyperHeaderCollection(), null) { }
+        public HyperStatus(HttpStatusCode code, HyperHeaderCollection headers) : this(code, headers, null) { }
+        public HyperStatus(HttpStatusCode code, object? body) : this(code, new HyperHeaderCollection(), body) { }
+        public HyperStatus(HttpStatusCode code, HyperHeaderCollection headers, object? body)
+        {
+            ArgumentNullException.ThrowIfNull(headers, nameof(headers));
+
+            Code = code;
+            Headers = headers;
+            Body = body;
+        }
+
+        public override string ToString() => $"{(int)Code} {Code}, {Headers.Count:N0} headers";
+    }
 }
