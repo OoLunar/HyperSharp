9916441c47a9fc17d9ec49e4a1e01e28a7ae573f
Lunar Starstrum <lunar@forsaken-borders.net>
2023-07-29 01:26:38 -0500

    Start on parsing tests only to subsequently remember core design flaws

--- tests/Benchmarks/ConcurrentRequests.cs
+++ tests/Benchmarks/ConcurrentRequests.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
 
 namespace OoLunar.HyperSharp.Tests.Benchmarks
 {
@@ -22,108 +13,19 @@ namespace OoLunar.HyperSharp.Tests.Benchmarks
 
         public ConcurrentRequests()
         {
-            ServiceProvider serviceProvider = CreateServiceProvider();
+            ServiceProvider serviceProvider = Program.CreateServiceProvider();
 
             _client = serviceProvider.GetRequiredService<HttpClient>();
             _hyperServer = serviceProvider.GetRequiredService<HyperServer>();
         }
 
         [GlobalSetup]
-        public void SetupAsync() => _hyperServer.Start();
+        public void Setup() => _hyperServer.Start();
 
         [GlobalCleanup]
         public Task CleanupAsync() => _hyperServer.StopAsync();
 
         [WarmupCount(25), Benchmark]
         public Task<HttpResponseMessage> ConcurrentRequestsTestAsync() => _client.GetAsync("/");
-
-        private static ServiceProvider CreateServiceProvider()
-        {
-            IServiceCollection services = new ServiceCollection();
-            services.AddSingleton<IConfiguration>(services => new ConfigurationBuilder()
-                .AddJsonFile("config.json", true, true)
-#if DEBUG
-                .AddJsonFile("config.debug.json", true, true)
-#endif
-                .AddEnvironmentVariables("HyperSharp_")
-                .Build());
-
-            services.AddLogging(logger =>
-            {
-                const string loggingFormat = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level:u4}] {SourceCont`t}: {Message:lj}{NewLine}{Exception}";
-                IConfiguration configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
-                LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
-                    .MinimumLevel.Is(configuration.GetValue("logging:level", LogEventLevel.Debug))
-                    .WriteTo.Console(outputTemplate: loggingFormat, formatProvider: CultureInfo.InvariantCulture, theme: new AnsiConsoleTheme(new Dictionary<ConsoleThemeStyle, string>
-                    {
-                        [ConsoleThemeStyle.Text] = "\x1b[0m",
-                        [ConsoleThemeStyle.SecondaryText] = "\x1b[90m",
-                        [ConsoleThemeStyle.TertiaryText] = "\x1b[90m",
-                        [ConsoleThemeStyle.Invalid] = "\x1b[31m",
-                        [ConsoleThemeStyle.Null] = "\x1b[95m",
-                        [ConsoleThemeStyle.Name] = "\x1b[93m",
-                        [ConsoleThemeStyle.String] = "\x1b[96m",
-                        [ConsoleThemeStyle.Number] = "\x1b[95m",
-                        [ConsoleThemeStyle.Boolean] = "\x1b[95m",
-                        [ConsoleThemeStyle.Scalar] = "\x1b[95m",
-                        [ConsoleThemeStyle.LevelVerbose] = "\x1b[34m",
-                        [ConsoleThemeStyle.LevelDebug] = "\x1b[90m",
-                        [ConsoleThemeStyle.LevelInformation] = "\x1b[36m",
-                        [ConsoleThemeStyle.LevelWarning] = "\x1b[33m",
-                        [ConsoleThemeStyle.LevelError] = "\x1b[31m",
-                        [ConsoleThemeStyle.LevelFatal] = "\x1b[97;91m"
-                    }))
-                    .WriteTo.File(
-                        $"logs/{DateTime.Now.ToUniversalTime().ToString("yyyy'-'MM'-'dd' 'HH'.'mm'.'ss", CultureInfo.InvariantCulture)}.log",
-                        rollingInterval: RollingInterval.Day,
-                        outputTemplate: loggingFormat,
-                        formatProvider: CultureInfo.InvariantCulture
-                    );
-
-                // Allow specific namespace log level overrides, which allows us to hush output from things like the database basic SELECT queries on the Information level.
-                foreach (IConfigurationSection logOverride in configuration.GetSection("logging:overrides").GetChildren())
-                {
-                    if (logOverride.Value is null || !Enum.TryParse(logOverride.Value, out LogEventLevel logEventLevel))
-                    {
-                        continue;
-                    }
-
-                    loggerConfiguration.MinimumLevel.Override(logOverride.Key, logEventLevel);
-                }
-
-                logger.AddSerilog(loggerConfiguration.CreateLogger());
-            });
-
-            services.AddHyperSharp((services, hyperConfiguration) =>
-            {
-                IConfiguration configuration = services.GetRequiredService<IConfiguration>();
-                string? host = configuration.GetValue("server:address", "localhost")?.Trim();
-                if (string.IsNullOrWhiteSpace(host))
-                {
-                    throw new ArgumentException("The server address cannot be null or whitespace.", nameof(host));
-                }
-
-                if (!IPAddress.TryParse(host, out IPAddress? address))
-                {
-                    IPAddress[] addresses = Dns.GetHostAddresses(host);
-                    address = addresses.Length != 0 ? addresses[0] : throw new InvalidOperationException("The server address could not be resolved to an IP address.");
-                }
-
-                hyperConfiguration.ListeningEndpoint = new IPEndPoint(address, configuration.GetValue("server:port", 8080));
-                hyperConfiguration.AddResponders(typeof(Program).Assembly);
-            });
-
-            services.AddSingleton(services =>
-            {
-                HyperConfiguration hyperConfiguration = services.GetRequiredService<HyperConfiguration>();
-                return new HttpClient()
-                {
-                    BaseAddress = new Uri($"http://{hyperConfiguration.ListeningEndpoint}/"),
-                    DefaultRequestHeaders = { { "User-Agent", $"HyperSharp/{typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion} Github" } }
-                };
-            });
-
-            return services.BuildServiceProvider();
-        }
     }
 }
