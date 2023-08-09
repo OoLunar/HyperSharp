7d5fd413206a036f0b67667a4a8ee6e384ccfe91
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-09 15:13:37 -0500

    Sync config properties and add helpful overloads

--- src/HyperSharp/HyperConfiguration.cs
+++ src/HyperSharp/HyperConfiguration.cs
@@ -13,13 +13,13 @@ namespace OoLunar.HyperSharp
 {
     public sealed record HyperConfiguration
     {
-        public ValueTaskResponderDelegate<HyperContext, HyperStatus> Responders { get; init; }
-        public IPEndPoint ListeningEndpoint { get; init; }
-        public JsonSerializerOptions JsonSerializerOptions { get; init; }
-        public TimeSpan Timeout { get; init; } = TimeSpan.FromSeconds(30);
+        public string ServerName { get; init; }
         public int MaxHeaderSize { get; init; }
         public Uri Host { get; init; }
-        public string ServerName { get; init; }
+        public TimeSpan Timeout { get; init; } = TimeSpan.FromSeconds(30);
+        public IPEndPoint ListeningEndpoint { get; init; }
+        public JsonSerializerOptions JsonSerializerOptions { get; init; }
+        public ValueTaskResponderDelegate<HyperContext, HyperStatus> RespondersDelegate { get; init; }
 
         internal HyperConfiguration(IServiceCollection serviceDescriptors, HyperConfigurationBuilder builder)
         {
@@ -43,12 +43,12 @@ namespace OoLunar.HyperSharp
 
             if (!responderCompiler.IsSynchronous())
             {
-                Responders = responderCompiler.CompileAsyncResponders<HyperContext, HyperStatus>(serviceProvider);
+                RespondersDelegate = responderCompiler.CompileAsyncResponders<HyperContext, HyperStatus>(serviceProvider);
                 return;
             }
 
             ResponderDelegate<HyperContext, HyperStatus> synchronousResponders = responderCompiler.CompileResponders<HyperContext, HyperStatus>(serviceProvider);
-            Responders = (HyperContext context, CancellationToken cancellationToken) => ValueTask.FromResult(synchronousResponders(context, cancellationToken));
+            RespondersDelegate = (HyperContext context, CancellationToken cancellationToken) => ValueTask.FromResult(synchronousResponders(context, cancellationToken));
         }
     }
 }
--- src/HyperSharp/Setup/HyperConfigurationBuilder.cs
+++ src/HyperSharp/Setup/HyperConfigurationBuilder.cs
@@ -2,18 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
+using OoLunar.HyperSharp.Responders;
 
 namespace OoLunar.HyperSharp.Setup
 {
     public sealed class HyperConfigurationBuilder
     {
-        public IPEndPoint ListeningEndpoint { get; set; } = new(IPAddress.Any, 8080);
         public int MaxHeaderSize { get; set; } = 8192;
-        public List<Type> Responders { get; set; } = new();
-        public string JsonSerializerOptionsName { get; set; } = "HyperSharp";
         public string ServerName { get; set; } = "HyperSharp";
+        public string JsonSerializerOptionsName { get; set; } = "HyperSharp";
+        public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(30);
+        public IPEndPoint ListeningEndpoint { get; set; } = new(IPAddress.Any, 8080);
+        public List<Type> Responders { get; set; } = new();
 
         public void AddResponders(Assembly assembly) => Responders.AddRange(assembly.GetTypes());
         public void AddResponders(IEnumerable<Type> responders) => Responders.AddRange(responders);
+        public void AddResponders(params Type[] responders) => Responders.AddRange(responders);
+        public void AddResponder<T>() where T : IResponderBase => Responders.Add(typeof(T));
     }
 }
