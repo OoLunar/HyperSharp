7f3f84211611760fb4163a9169b612e1f70e2d88
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-11 19:40:36 -0500

    Add Exception overload for Error

--- src/HyperSharp.Results/Error.cs
+++ src/HyperSharp.Results/Error.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -8,19 +9,22 @@ namespace OoLunar.HyperSharp.Results
     [JsonConverter(typeof(ErrorJsonConverter))]
     public record Error
     {
+        private static readonly Error[] _empty = Array.Empty<Error>();
+
         public string Message { get; init; }
         public IEnumerable<Error> Errors { get; init; }
+        public Exception? Exception { get; init; }
 
         public Error()
         {
             Message = "<No error message provided>";
-            Errors = Enumerable.Empty<Error>();
+            Errors = _empty;
         }
 
         public Error(string message)
         {
             Message = message;
-            Errors = Enumerable.Empty<Error>();
+            Errors = _empty;
         }
 
         public Error(string message, Error error)
@@ -32,6 +36,48 @@ namespace OoLunar.HyperSharp.Results
         public Error(string message, IEnumerable<Error> errors)
         {
             Message = message;
+            Errors = errors;
+        }
+
+        public Error(Exception exception)
+        {
+            Message = exception.Message;
+            Exception = exception;
+            Errors = _empty;
+        }
+
+        public Error(Exception exception, Error error)
+        {
+            Message = exception.Message;
+            Exception = exception;
+            Errors = Enumerable.Repeat(error, 1);
+        }
+
+        public Error(Exception exception, IEnumerable<Error> errors)
+        {
+            Message = exception.Message;
+            Exception = exception;
+            Errors = errors;
+        }
+
+        public Error(string message, Exception exception)
+        {
+            Message = message;
+            Exception = exception;
+            Errors = _empty;
+        }
+
+        public Error(string message, Exception exception, Error error)
+        {
+            Message = message;
+            Exception = exception;
+            Errors = Enumerable.Repeat(error, 1);
+        }
+
+        public Error(string message, Exception exception, IEnumerable<Error> errors)
+        {
+            Message = message;
+            Exception = exception;
             Errors = errors;
         }
     }
