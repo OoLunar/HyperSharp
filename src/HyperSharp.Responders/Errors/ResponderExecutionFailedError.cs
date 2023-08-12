7b5bcae42441af32e91ba3ad638a6a958ac0d1d7
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-11 21:58:56 -0500

    Fix compiler warning

--- src/HyperSharp.Responders/Errors/ResponderExecutionFailedError.cs
+++ src/HyperSharp.Responders/Errors/ResponderExecutionFailedError.cs
@@ -1,11 +1,6 @@
-using System;
 using OoLunar.HyperSharp.Results;
 
 namespace OoLunar.HyperSharp.Responders.Errors
 {
-    public sealed record ResponderExecutionFailedError : Error
-    {
-        public Exception? Exception { get; init; }
-        public ResponderExecutionFailedError(Exception? exception = null) => Exception = exception;
-    }
+    public sealed record ResponderExecutionFailedError : Error;
 }
