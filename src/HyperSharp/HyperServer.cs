7de7414ef56c94305570504814bec2916ba238e5
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-13 21:25:45 -0500

    Remove Source Generation
    Despite the convenience of source generation, it's evident that its current state does not align with an efficient and satisfactory development experience. This discrepancy stems from various factors including inadequate support from major IDEs/editors, confinement to netstandard2.0, and unexpected behavior exhibited by commands such as `dotnet build`. As a result, considering its present condition, the suitability of sgen for integration into this project is questionable and is left undesired.

--- src/HyperSharp/HyperServer.cs
+++ src/HyperSharp/HyperServer.cs
@@ -12,20 +12,51 @@ using HyperSharp.Setup;
 
 namespace HyperSharp
 {
+    /// <summary>
+    /// Represents a high-level HTTP server for handling incoming connections and requests.
+    /// </summary>
     public sealed class HyperServer
     {
+        /// <summary>
+        /// Gets the configuration settings for the <see cref="HyperServer"/>.
+        /// </summary>
         public HyperConfiguration Configuration { get; init; }
-        private readonly ILogger<HyperServer> _logger;
-        private readonly ConcurrentDictionary<Ulid, HyperConnection> _openConnections = new();
-        private readonly ConcurrentStack<CancellationTokenSource> _cancellationTokenSources = new();
-        private CancellationTokenSource? _mainCancellationTokenSource;
 
+        /// <summary>
+        /// If no logger was specified in the constructor, this will be a <see cref="NullLogger{T}"/> instance.
+        /// </summary>
+        private readonly ILogger<HyperServer> _logger;
+
+        /// <summary>
+        /// Dictionary to store currently open connections by their unique IDs.
+        /// </summary>
+        private readonly ConcurrentDictionary<Ulid, HyperConnection> _openConnections = new();
+
+        /// <summary>
+        /// Stack of cancellation token sources for reuse in handling connections.
+        /// </summary>
+        private readonly ConcurrentStack<CancellationTokenSource> _cancellationTokenSources = new();
+
+        /// <summary>
+        /// The main cancellation token source for controlling the server's lifecycle.
+        /// </summary>
+        private CancellationTokenSource? _mainCancellationTokenSource;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HyperServer"/> class with the specified configuration and optional logger.
+        /// </summary>
+        /// <param name="configuration">The configuration settings for the server.</param>
+        /// <param name="logger">Optional logger to log server activities.</param>
         public HyperServer(HyperConfiguration configuration, ILogger<HyperServer>? logger = null)
         {
             Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _logger = logger ?? NullLogger<HyperServer>.Instance;
         }
 
+        /// <summary>
+        /// Starts the server, listening for incoming connections.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to stop the server.</param>
+        /// <exception cref="InvalidOperationException">Thrown if the server is already running.</exception>
         public void Start(CancellationToken cancellationToken = default)
         {
             if (_mainCancellationTokenSource is not null)
@@ -70,6 +101,11 @@ namespace HyperSharp
             HyperLogging.ServerStarted(_logger, Configuration.ListeningEndpoint, null);
         }
 
+        /// <summary>
+        /// Stops the server, blocking until all connections have been closed.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the server is not running.</exception>
         public async Task StopAsync()
         {
             if (_mainCancellationTokenSource is null)
@@ -82,6 +118,11 @@ namespace HyperSharp
             _mainCancellationTokenSource = null;
         }
 
+        /// <summary>
+        /// Listens for incoming connections asynchronously, accepting them and throwing them onto the async thread pool.
+        /// </summary>
+        /// <param name="listener">The TCP listener instance to accept connections from.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         private async Task ListenForConnectionsAsync(TcpListener listener)
         {
             while (!_mainCancellationTokenSource!.IsCancellationRequested)
@@ -91,6 +132,11 @@ namespace HyperSharp
             }
         }
 
+        /// <summary>
+        /// Handles an incoming connection asynchronously, parsing the HTTP headers and executing any registered responders.
+        /// </summary>
+        /// <param name="client">The connected TCP client.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         private async Task HandleConnectionAsync(TcpClient client)
         {
             HyperConnection connection = new(client, this);
--- src/HyperSharp/Protocol/HyperStatus.cs
+++ src/HyperSharp/Protocol/HyperStatus.cs
@@ -1,29 +1,20 @@
 using System;
 using System.Diagnostics;
 using System.Net;
-using HyperSharp.SourceGenerators;
 
 namespace HyperSharp.Protocol
 {
     [DebuggerDisplay("{ToString(),nq}")]
-    public readonly partial record struct HyperStatus
+    public readonly record struct HyperStatus
     {
         public HttpStatusCode Code { get; init; }
         public HyperHeaderCollection Headers { get; init; }
         public object? Body { get; init; }
 
         public HyperStatus() : this(HttpStatusCode.InternalServerError, new HyperHeaderCollection(), null) { }
-
-        [GenerateStatusCreation]
         public HyperStatus(HttpStatusCode code) : this(code, new HyperHeaderCollection(), null) { }
-
-        [GenerateStatusCreation]
         public HyperStatus(HttpStatusCode code, object? body) : this(code, new HyperHeaderCollection(), body) { }
-
-        [GenerateStatusCreation]
         public HyperStatus(HttpStatusCode code, HyperHeaderCollection headers) : this(code, headers, null) { }
-
-        [GenerateStatusCreation]
         public HyperStatus(HttpStatusCode code, HyperHeaderCollection headers, object? body)
         {
             ArgumentNullException.ThrowIfNull(headers, nameof(headers));
