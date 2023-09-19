595c14860d624089d416c5da29f5126cfc081ee4
Lunar Starstrum <lunar@forsaken-borders.net>
2023-09-19 17:37:08 -0500

    Add benchmarks against other providers

--- benchmarks/Benchmarks/HttpBenchmarks.cs
+++ benchmarks/Benchmarks/HttpBenchmarks.cs
@@ -1,10 +1,14 @@
 using System;
-using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
-using HyperSharp.Protocol;
-using HyperSharp.Results;
+using GenHTTP.Api.Infrastructure;
+using GenHTTP.Api.Protocol;
+using GenHTTP.Engine;
+using GenHTTP.Modules.IO.Providers;
+using GenHTTP.Modules.IO.Strings;
+using GenHTTP.Modules.Practices;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HyperSharp.Benchmarks.Cases
@@ -12,10 +16,12 @@ namespace HyperSharp.Benchmarks.Cases
     [JsonExporterAttribute.Brief]
     public class HttpBenchmarks
     {
-        private static readonly byte[] _headers = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate, br\r\nDNT: 1\r\nAlt-Used: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nSec-Fetch-Dest: document\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-Site: cross-site\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nTE: trailers\r\n\r\n"u8.ToArray();
-        private static readonly Uri _localhost = new("http://localhost:8080/");
         private readonly HttpClient _client;
+
         private readonly HyperServer _hyperServer;
+        private readonly HttpListener _httpListener;
+        private readonly IServerHost _genHttpServer = Host.Create().Defaults().Handler(new ContentProviderBuilder().Resource(new StringResource("Hello World", "/", FlexibleContentType.Get(ContentType.TextPlain, "UTF-8"), DateTime.UtcNow))).Port(8082);
+        // private readonly IAspnetServer _aspnetServer = new AspnetServer();
 
         public HttpBenchmarks()
         {
@@ -23,22 +29,49 @@ namespace HyperSharp.Benchmarks.Cases
 
             _client = serviceProvider.GetRequiredService<HttpClient>();
             _hyperServer = serviceProvider.GetRequiredService<HyperServer>();
+            _httpListener = new();
+            _httpListener.Prefixes.Clear();
+            _httpListener.Prefixes.Add("http://localhost:8081/");
         }
 
         [GlobalSetup]
-        public void Setup() => _hyperServer.Start();
+        public void Setup()
+        {
+            _hyperServer.Start();
+            _httpListener.Start();
+            _ = Task.Run(async () =>
+            {
+                byte[] helloWorld = "Hello World!"u8.ToArray();
+                while (_httpListener.IsListening)
+                {
+                    HttpListenerContext context = await _httpListener.GetContextAsync();
+                    context.Response.StatusCode = 200;
+                    context.Response.Close(helloWorld, false);
+                }
+            });
+            _genHttpServer.Start();
+            // _aspnetServer.Start();
+        }
 
-        [GlobalCleanup]
-        public async Task CleanupAsync() => await _hyperServer.StopAsync();
+        [WarmupCount(5), Benchmark(Baseline = true)]
+        public Task HyperTestAsync() => _client.GetAsync("http://localhost:8080/");
 
         [WarmupCount(5), Benchmark]
-        public Task HttpClientTestAsync() => _client.GetAsync(_localhost);
+        public Task HttpListenerTestAsync() => _client.GetAsync("http://localhost:8081/");
 
-        [WarmupCount(5), Benchmark(41)]
-        public ValueTask<Result<HyperContext>> ParseHeadersTestAsync()
+        [WarmupCount(5), Benchmark]
+        public Task GenHttpTestAsync() => _client.GetAsync("http://localhost:8082/");
+
+        [GlobalCleanup]
+        public async Task CleanupAsync()
         {
-            HyperConnection connection = new(new MemoryStream(_headers), _hyperServer);
-            return HyperHeaderParser.TryParseHeadersAsync(_hyperServer.Configuration.MaxHeaderSize, connection, default);
+            await _hyperServer.StopAsync();
+            _httpListener.Stop();
+            _genHttpServer.Stop();
+            // _aspnetServer.Stop();
         }
+
+        // [WarmupCount(5), Benchmark]
+        // public Task AspnetTestAsync() => _client.GetAsync("http://localhost:8083/");
     }
 }
---
+++ benchmarks/Benchmarks/HyperBenchmarks.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using HyperSharp.Protocol;
+using HyperSharp.Results;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HyperSharp.Benchmarks.Cases
+{
+    [JsonExporterAttribute.Brief]
+    public class HyperBenchmarks
+    {
+        private static readonly byte[] _headers = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate, br\r\nDNT: 1\r\nAlt-Used: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nSec-Fetch-Dest: document\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-Site: cross-site\r\nPragma: no-cache\r\nCache-Control: no-cache\r\nTE: trailers\r\n\r\n"u8.ToArray();
+        private static readonly Uri _localhost = new("http://localhost:8080/");
+        private readonly HttpClient _client;
+        private readonly HyperServer _hyperServer;
+
+        public HyperBenchmarks()
+        {
+            ServiceProvider serviceProvider = Program.CreateServiceProvider();
+
+            _client = serviceProvider.GetRequiredService<HttpClient>();
+            _hyperServer = serviceProvider.GetRequiredService<HyperServer>();
+        }
+
+        [GlobalSetup]
+        public void Setup() => _hyperServer.Start();
+
+        [GlobalCleanup]
+        public async Task CleanupAsync() => await _hyperServer.StopAsync();
+
+        [WarmupCount(5), Benchmark]
+        public Task HttpClientTestAsync() => _client.GetAsync(_localhost);
+
+        [WarmupCount(5), Benchmark(41)]
+        public ValueTask<Result<HyperContext>> ParseHeadersTestAsync()
+        {
+            HyperConnection connection = new(new MemoryStream(_headers), _hyperServer);
+            return HyperHeaderParser.TryParseHeadersAsync(_hyperServer.Configuration.MaxHeaderSize, connection, default);
+        }
+    }
+}
